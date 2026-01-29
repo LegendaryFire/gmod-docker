823b7bb4f9c20884359936d17df7582465f29515
LegendaryFire <tristan.balon@outlook.com>
2026-01-28 22:09:31 -0800

    Improve permission handling on bind mounted directories

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,13 +1,48 @@
 #!/bin/sh
 set -euo pipefail
 
-ls -lha /data
+warn() {
+  if [ -t 2 ]; then
+    printf '\e[1;33mWARNING:\e[0m %s\n' "$*" >&2
+  else
+    printf 'WARNING: %s\n' "$*" >&2
+  fi
+}
+
+error() {
+  if [ -t 2 ]; then
+    printf '\e[1;31mERROR:\e[0m %s\n' "$*" >&2
+  else
+    printf 'ERROR: %s\n' "$*" >&2
+  fi
+}
+
+has_permission() {
+  [ -d "$1" ] && [ -w "$1" ] && [ -x "$1" ]
+}
+
+DEFAULT_DIR="/data/defaults"
+CONFIG_DIR="/data/config"
+CACHE_DIR="/data/garrysmod/cache"
+
+# Validate correct file permissions for the config and cache directories.
+if ! has_permission "$CONFIG_DIR"; then
+  error "Insufficient permissions to configuration directory $CONFIG_DIR. Exiting."
+  sleep 5
+  exit 1
+fi
+
+if ! has_permission "$CACHE_DIR"; then
+  error "Insufficient permissions to cache directory $CACHE_DIR. Exiting."
+  sleep 5
+  exit 1
+fi
 
 # Create defaults, only if missing. Then link the server.cfg and users.txt to their corresponding paths.
-[ -f /data/config/server.cfg ] || cp /data/defaults/server.cfg /data/config/server.cfg
-[ -f /data/config/users.txt  ] || cp /data/defaults/users.txt  /data/config/users.txt
-ln -sf /data/config/server.cfg /data/garrysmod/cfg/server.cfg
-ln -sf /data/config/users.txt  /data/garrysmod/settings/users.txt
+[ -f "$CONFIG_DIR/server.cfg" ] || cp "$DEFAULT_DIR/server.cfg" "$CONFIG_DIR/server.cfg"
+[ -f "$CONFIG_DIR/users.txt"  ] || cp "$DEFAULT_DIR/users.txt"  "$CONFIG_DIR/users.txt"
+ln -sf "$CONFIG_DIR/server.cfg" /data/garrysmod/cfg/server.cfg
+ln -sf "$CONFIG_DIR/users.txt"  /data/garrysmod/settings/users.txt
 
 # Set default environment variable values.
 PORT="${PORT:-27015}"
